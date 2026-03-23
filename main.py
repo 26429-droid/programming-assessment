437675260aa3fa9e79c78115658b70b023efa178
Artharva <26429@wc.school.nz>
2026-03-23 14:22:27 +1300

    Palmerston North dictionary finished, working on Kapiti divtionary.

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ jungle = [
         "Name": "Parrot",
         "Health":random.randint(25,30),
         "Special Abilty": "Gust",
-        "Spawnrate":random.randint(20,50)
+        "Spawnrate":random.randint(20,50),
     },
 
     {
@@ -50,6 +50,38 @@ jungle = [
         "Name": "Bear",
         'Health':random.randint(90,100),
         "Special Abilty": "Roar",
-        "Spawnrate":random.randint(1,2)
+        "Spawnrate":random.randint(1,2),
     },
 ]
+palmerston_north = [
+    {
+        "Type":"Tool",
+        "Name":"Car",
+        "Durabilty":random.randint(500,1000),
+        "Special Abilty":"None",
+    },
+
+]
+
+kapiti= [
+    {
+        "Type":"Tool",
+        "Name":"Fishing Rod",
+        "Durabilty":random.randint(500,700),
+        "Special Abilty": "None",
+
+    },
+
+    {
+        "Type":"Animal",
+        "Name":"Long Finned Eel",
+        "Health":random.randint(25,40),
+        "Special Abilty":"Splash",
+        "Spawnrate":random.randint(75.100),
+
+    },
+
+
+
+]
+
